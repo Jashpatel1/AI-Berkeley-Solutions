ee0f0dac4f68083ca3dedc6501cfba1b410a4a95
Jashpatel1 <patel.5@iitj.ac.in>
2020-10-15 13:40:36 +0530

    upadted eatdots heursitic

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -497,18 +497,11 @@ def foodHeuristic(state, problem):
         foodList.remove(position)
 
     distance = []
-    temp = []
     for food in foodList:
-        distance.append([util.manhattanDistance(food,position),food])
-    next = min(distance)
+        distance.append(util.manhattanDistance(food,position))
+    next = max(distance)
     distance.remove(next)
-    length = len(distance)
-    if length == 0:
-        return next[0]
-    else:
-        for food in foodList:
-            temp.append(util.manhattanDistance(food,next[1]))   
-        return next[0] + min(temp)
+    return next
 
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
